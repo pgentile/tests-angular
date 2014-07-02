8c2fcf2f669729147ca17ce686e8962478551774
Pierre Gentile <pierre.gentile.perso@gmail.com>
2014-07-02 22:48:48 +0200

    Simplification du serveur

--- server.js
+++ server.js
@@ -3,12 +3,7 @@
 var express = require('express');
 
 
-var app = express();
-
-// Servir l'appli
-app.use('/', express.static(__dirname + '/app'));
-
-// Servir les composants utilisés
-app.use('/static', express.static(__dirname + '/bower_components'));
-
-app.listen(3000);
+var app = express()
+  .use('/', express.static(__dirname + '/app'))
+  .use('/static', express.static(__dirname + '/bower_components'))
+  .listen(3000);
