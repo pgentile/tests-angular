ef74b979eda0bad2a599f37184f7162f8c57a78e
Pierre Gentile <pierre.gentile.perso@gmail.com>
2014-07-03 22:10:50 +0200

    On continue d'exploiter Angular : filtre et événements inter-contrôleurs

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,17 +1,49 @@
 'use strict';
 
-var dashboardApp = angular.module('dashboardApp', []);
-
-dashboardApp.controller('GraphController', function ($scope) {
-  $scope.graphs = [
-    {
-      title: 'CPU'
-    },
-    {
-      title: 'Mémoire'
-    },
-    {
-      title: 'Disques'
+var dashboardApp = angular.module('dashboardApp', [])
+  .factory('alerting', function () {
+    return window.alert;
+  })
+  .filter('upper', function () {
+    return function (value) {
+      return value.toUpperCase();
     }
-  ];
-});
+  })
+  .controller('GraphController', function ($scope, alerting) {
+    $scope.graphs = [
+      {
+        id: 4,
+        title: 'CPU'
+      },
+      {
+        id: 65,
+        title: 'Mémoire'
+      },
+      {
+        id: 17,
+        title: 'Disques'
+      }
+    ];
+  
+    $scope.alert = function (name) {
+      alerting('Nom actuel : ' + name);
+    };
+  
+  })
+  .controller('EventWatcherController', function ($scope) {
+    $scope.eventsAddedCount = 0;
+    
+    $scope.$on('newEventAdded', function () {
+      $scope.eventsAddedCount += 1;
+    });
+  })
+  .controller('EventController', function ($scope) {
+    $scope.events = [];
+    
+    $scope.add = function () {
+      var num = $scope.events.length + 1;
+      $scope.events.push('Event #' + num);
+      
+      $scope.$emit('newEventAdded');
+    };
+  });
