e261d0f1cb64a762ad290cd6cf6f3c65627caf4e
Pierre Gentile <pierre.gentile.perso@gmail.com>
2014-07-30 22:44:43 +0200

    Début de navbar qui fonctionne correctement

--- app/js/navbar.js
+++ app/js/navbar.js
@@ -2,12 +2,41 @@
 
 
 angular.module('navbar', [])
-  .directive('ttNavbar', function () {
+  .directive('navbar', function () {
     return {
       restrict: 'E',
       scope: {
         title: '@'
       },
+      transclude: true,
       templateUrl: '/templates/navbar.html'
+    };
+  })
+  .directive('navbarList', function () {
+    return {
+      restrict: 'E',
+      scope: {},
+      transclude: true,
+      template: '<ul class="nav navbar-nav" ng-transclude></ul>'
+    };
+  })
+  .directive('navbarLink', function ($compile) {
+    return {
+      restrict: 'E',
+      scope: {
+        url: '@',
+        active: '='
+      },
+      transclude: true,
+      link: function (scope, element, attrs, controller, transclude) {
+        // On fait sauter l'élément transclus en compilant
+        var item = $compile(
+          '<li ng-class="{ active: active }"><a href="{{ url }}" ng-transclude></a></li>',
+          transclude
+        );
+        item(scope, function (clonedElement, innerScope) {
+          element.replaceWith(clonedElement);
+        });
+      }
     };
   });
