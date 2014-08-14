b08da2950f9742365f76114fb57c9905886d6e3e
Pierre Gentile <pierre.gentile.perso@gmail.com>
2014-08-14 22:14:34 +0200

    Utilisation de Grunt pour lancer un serveur de tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,49 +2,58 @@
 
 module.exports = function(grunt) {
   
+  // Analyser les perfs du build Grunt
+  // require('time-grunt')(grunt);
+  
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    clean: {
+      dist: './dist'
+    },
     jshint: {
-      files: [
-        'Gruntfile.js',
-        'app/js/**/*.js',
-      ],
+      files: ['Gruntfile.js', 'app/js/**/*.js'],
       options: {
-        node: true,
-        browser: true,
-        unused: true,
-        strict: true,
-        esnext: true,
-        bitwise: true,
-        camelcase: true,
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        indent: 2,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        quotmark: 'single',
-        regexp: true,
-        undef: true,
-        trailing: true,
-        smarttabs: true,
-        globals: {
-          angular: false,
-          d3: false
+        jshintrc: 'jshintrc.json'
+      }
+    },
+    copy: {
+      main: {
+        files: [
+          {
+            expand: true,
+            cwd: './app',
+            src: '**',
+            dest: './dist',
+          }
+        ]
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 3000,
+          keepalive: true,
+          middleware: function (connect) {
+            return [
+              connect().use('/static', connect.static('./bower_components')),
+              connect.static('./dist')
+            ];
+          }
         }
       }
     }
   });
   
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
   
-  /*
-  grunt.registerTask('test', 'Test', function (target) {
-    grunt.log.warn("Salut");
-  });
-  */
+  grunt.registerTask('build', ['clean', 'copy'])
   
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('serve', ['build', 'connect:server'])
+  
+  grunt.registerTask('default', ['build']);
   
 };
--- server.js
+++
@@ -1,9 +0,0 @@
-'usr strict';
-
-var express = require('express');
-
-
-var app = express()
-  .use('/', express.static(__dirname + '/app'))
-  .use('/static', express.static(__dirname + '/bower_components'))
-  .listen(3000);
