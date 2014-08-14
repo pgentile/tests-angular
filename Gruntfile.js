84bab0e739b39fbfce73e89cd23bbc43ca7e5db0
Pierre Gentile <pierre.gentile.perso@gmail.com>
2014-08-14 19:43:38 +0200

    Initialisation d'un build avec Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = function(grunt) {
+  
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      files: [
+        'Gruntfile.js',
+        
+      ],
+      options: {
+        node: true,
+        browser: true,
+        unused: true,
+        strict: true,
+        esnext: true,
+        bitwise: true,
+        camelcase: true,
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        indent: 2,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        quotmark: 'single',
+        regexp: true,
+        undef: true,
+        trailing: true,
+        smarttabs: true,
+      }
+    }
+  });
+  
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  
+  /*
+  grunt.registerTask('test', 'Test', function (target) {
+    grunt.log.warn("Salut");
+  });
+  */
+  
+  grunt.registerTask('default', ['jshint']);
+  
+};
