c1da3f9d5c7c43be36ed92c821d08f171db2444e
Pierre Gentile <pierre.gentile.perso@gmail.com>
2014-08-24 14:04:23 +0200

    Suppression des logs

--- app/js/tree.js
+++ app/js/tree.js
@@ -2,42 +2,34 @@
 
 
 angular.module('tree', [])
-  .directive('tree', function ($compile, $log) {
+  .directive('tree', function ($compile) {
     return {
       restrict: 'AE',
       controller: function () {
-        $log.info('tree - Creating controller');
+        this.template = null;
       },
       compile: function (element) {
-        $log.info('tree - Compiling', element.get(0));
-        
         var elementClone = element.children().clone();
         var compiled = $compile(elementClone);
         
         return function (scope, element, attrs, controller) {
-          $log.info('tree - Linking', element.get(0));
           controller.template = compiled;
           
         };
       }
     };
   })
-  .directive('callTree', function ($log) {
+  .directive('callTree', function () {
     return {
       restrict: 'AE',
       require: '^tree',
       scope: true,
       compile: function (element, attrs) {
-        $log.info('callTree - Compiling', element.get(0));
-        
         if (angular.isUndefined(attrs.rebind)) {
           throw new Error('Attribut rebind non défini');
         }
         
         return function (scope, element, attrs, controller) {
-          $log.info('callTree - Linking', element.get(0));
-          $log.info('callTree - Controller =', controller);
-
           // Toute la magie est ici : on écrase dans le scope descendant
           // la variable de scope sur laquelle appliquer la récursion.
           // scope[attrs.varName] = scope.$eval(attrs.value);
